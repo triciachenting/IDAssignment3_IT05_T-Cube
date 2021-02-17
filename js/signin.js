6c3c0057cad7802b3b9da722ee03ce6f4db46e3d
JieShengNP <s10205049@connect.np.edu.sg>
2021-02-17 22:48:41 +0800

    Add sign-in validations and user feedbacks

--- js/signin.js
+++ js/signin.js
@@ -1,39 +1,80 @@
 //Sign In Function
 function signinGetValue() {
+    $('p.si-invalid').css("opacity", 0);
+    document.getElementById('si-button').disabled = true;
     var signupUsername = document.getElementById("si-username").value.toUpperCase();
     var signupPassword = document.getElementById("si-password").value;
-    $.ajax({
-        "async": true,
-        "crossDomain": true,
-        "url": "https://friesforguys-c324.restdb.io/rest/accounts",
-        "method": "GET",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": "602ac81a5ad3610fb5bb6085",
-            "cache-control": "no-cache"
-        }
-    }).done(function (response) {
-        if (response.length != 0) {
-            for (i = 0; i < response.length; i++) {
-                if (signupUsername == response[i].Username && signupPassword == response[i].Password) {
-                    window.localStorage.setItem("AccountInfo", JSON.stringify(response[i]));
-                    setTimeout(function () {
-                        //Redirect to game website.
-                    }, 1000);
-                    break;
-                } else if ((i + 1) == response.length) {
-                    setTimeout(function () {
-                        //#TODO: Alert user does not exist
-                        alert("Account does not exist!");
-                    }, 1000);
+    if (signupUsername == "") {
+        $('#si-username').addClass('is-invalid');
+        $('.si-username-if').text('Username cannot be empty!');
+        currentUsername = signupUsername;
+        var checkUsername = setInterval(function () {
+            if (document.getElementById("si-username").value.toUpperCase() != currentUsername) {
+                $('#si-username').removeClass('is-invalid');
+                $('.si-username-if').text('');
+                document.getElementById('si-button').disabled = false;
+                clearInterval(checkUsername);
+            }
+        }, 100);
+    }
+    if (signupPassword == "") {
+        $('#si-password').addClass('is-invalid');
+        $('.si-password-if').text('Password cannot be empty!');
+        currentPassword = signupPassword;
+        var checkPassword = setInterval(function () {
+            if (document.getElementById("si-password").value.toUpperCase() != currentPassword) {
+                $('#si-password').removeClass('is-invalid');
+                $('.si-password-if').text('');
+                document.getElementById('si-button').disabled = false;
+                clearInterval(checkPassword);
+            }
+        }, 100);
+    }
+    if (signupUsername != "" && signupPassword != "") {
+        $.ajax({
+            "async": true,
+            "crossDomain": true,
+            "url": "https://friesforguys-c324.restdb.io/rest/accounts",
+            "method": "GET",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": "602ac81a5ad3610fb5bb6085",
+                "cache-control": "no-cache"
+            }
+        }).done(function (response) {
+            if (response.length != 0) {
+                for (i = 0; i < response.length; i++) {
+                    if (signupUsername == response[i].Username && signupPassword == response[i].Password) {
+                        window.localStorage.setItem("AccountInfo", JSON.stringify(response[i]));
+                        setTimeout(function () {
+                            //Redirect to game website.
+                        }, 1000);
+                        break;
+                    } else if ((i + 1) == response.length) {
+                        setTimeout(function () {
+                            $('#si-username').addClass('is-invalid');
+                            $('#si-password').addClass('is-invalid');
+                            $('p.si-invalid').css("opacity", 1);
+                            currentUsername = signupUsername;
+                            currentPassword = signupPassword;
+                            var checkBoth = setInterval(function () {
+                                if (document.getElementById("si-username").value.toUpperCase() != currentUsername || document.getElementById("si-password").value != currentPassword) {
+                                    $('#si-username').removeClass('is-invalid');
+                                    $('#si-password').removeClass('is-invalid');
+                                    document.getElementById('si-button').disabled = false;
+                                    clearInterval(checkBoth);
+                                }
+                            }, 100);
+                        }, 1000);
+                    }
                 }
             }
-        }
-        else {
-            //#TODO: No accounts exist
-            alert("No accounts currently exist.");
-        }
-    });
+            else {
+                //#TODO: No accounts exist
+                alert("No accounts currently exist.");
+            }
+        });
+    }
 }
 
 //Clear Local Storage on Load
