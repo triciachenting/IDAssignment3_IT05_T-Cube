70bad899f6e44fed07ffd0f0e4a89ba817b130e3
JieShengNP <s10205049@connect.np.edu.sg>
2021-02-19 05:11:15 +0800

    Update navbar for all sites.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 tagNoAccount = '<a class="nav-link" href="signin.html">Sign In</a>\
                     <a class="nav-link" href="signup.html">Sign Up</a>';
-tagWithAccount = '<a class="nav-link" href="cardmemory.html">Card Memory</a>\
-                    <a class="nav-link" href="simongame.html">Simons Game</a>\
-                    <a class="nav-link" href="#" onclick="logout();">Log Out</a>';
 tagOriginal = $('.index-nav-list').html();
 
 mainNoAccount = '<h1 class="cover-heading">Memory Games</h1>\
@@ -22,7 +19,11 @@ mainWithAccount = '<h1 class="cover-heading">Memory Games</h1>\
 
 function checkLocalStorage() {
     if (window.localStorage.getItem('AccountInfo')) {
-        $('.index-nav-list').html(tagOriginal + tagWithAccount);
+        username = JSON.parse(window.localStorage.getItem('AccountInfo')).Username;
+        tagWithAccount = `<a class="nav-link" href="cardmemory.html">Card Memory</a>\
+                    <a class="nav-link" href="simongame.html">Simons Game</a>\
+                    <a class="nav-link active" href="#" onclick="logout();">Account(${username})</a>\
+                    <a class="nav-link" href="#" onclick="logout();">Log Out</a>`;
         $('.index-main').html(mainWithAccount);
     } else {
         $('.index-nav-list').html(tagOriginal + tagNoAccount);
