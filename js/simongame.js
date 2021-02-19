26ede66109debac08c7ccd772c03a868608675d8
JieShengNP <s10205049@connect.np.edu.sg>
2021-02-19 18:25:03 +0800

    Add round progression in simongame

--- js/simongame.js
+++ js/simongame.js
@@ -4,11 +4,19 @@ setInterval(function(){
 
 let sequence = [];
 let playerSequence = [];
+let level = 0;
 
 function sgStartGame() {
     $('#sg-button').prop('disabled', true)
     $('#sg-button').text("Starting.....")
     setTimeout(() => {
         $('#sg-button').css('opacity', 0)
+        nextRound();
     }, 1000);
+}
+
+function nextRound(){
+    level += 1;
+    $('#all-tiles').css("pointer-events", "none");
+    $('#round-info').text(`Round ${level}`);
 }
