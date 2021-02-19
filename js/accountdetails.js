a1b13a342ed28b7646558f9a00a6169755b049f0
JieShengNP <s10205049@connect.np.edu.sg>
2021-02-19 12:44:33 +0800

    Add updating of user stats on account page

--- js/accountdetails.js
+++ js/accountdetails.js
@@ -10,27 +10,27 @@ function checkLocalStorageAccount() {
         mainHTML = '<div class="d-flex row mx-auto justify-content-center">\
                         <div class="col-sm-3 d-flex flex-column" id="scoreContainer" style="border: 4px solid black; background-color: rgb(223, 223, 223, 1); border-radius: 40px; color: black; ">\
                             <div class="score-item" style="margin-top: 10px;"><strong>Card Memory</strong></div>\
-                            <div class="score-item">Games Played:</div>\
-                            <div class="score-item">0</div>\
-                            <div class="score-item">High Score:</div>\
-                            <div class="score-item">8 moves</div>\
-                        </div>\
+                            <div class="score-item">Games Played:</div>'+
+                            `<div class="score-item">${JSON.parse(localStorage.getItem('AccountInfo')).Highscores[0].CardGameTotalPlayed}</div>` +
+                            '<div class="score-item">High Score:</div>' +
+                            `<div class="score-item">${JSON.parse(localStorage.getItem('AccountInfo')).Highscores[0].CardGameHighScore} moves</div>` +
+                            '</div>\
                         <div class="col-sm-1" style="background-color: transparent; height: 10px;"></div>\
                         <div class="col-sm-3 d-flex flex-column" id="scoreContainer" style="background-color: black; border: 2px solid rgb(223, 223, 223, 1); border-radius: 40px;">\
                             <div class="score-item" style="margin-top: 10px;"><strong>Simons Game</strong></div>\
-                            <div class="score-item">Games Played:</div>\
-                            <div class="score-item">0</div>\
-                            <div class="score-item">High Score:</div>\
-                            <div class="score-item">8</div>\
-                        </div>\
+                            <div class="score-item">Games Played:</div>'+
+                            `<div class="score-item">${JSON.parse(localStorage.getItem('AccountInfo')).Highscores[0].SimonGameTotalPlayed}</div>` +
+                            '<div class="score-item">High Score:</div>'+
+                            `<div class="score-item">${JSON.parse(localStorage.getItem('AccountInfo')).Highscores[0].SimonGameHighScore}</div>` +
+                        '</div>\
                     </div>'
-        
+
         $('.accountdetails-main').html(mainHTML)
-        
+
         $('#scoreContainer').height($('#scoreContainer').width())
-        $('.score-item').height($('#scoreContainer').width()/5)
-        setInterval(function(){
-            $('.score-item').css('font-size',$('#scoreContainer').width()/10);
+        $('.score-item').height($('#scoreContainer').width() / 5)
+        setInterval(function () {
+            $('.score-item').css('font-size', $('#scoreContainer').width() / 10);
         }, 100)
     } else {
         alert("You need to be logged in to see your profile!");
