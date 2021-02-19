736c70107f5fa1a0c180b69c3bfe0a9c8634c476
triciachenting <s10194120@connect.np.edu.sg>
2021-02-19 21:29:50 +0800

    updated card.js

--- js/card.js
+++ js/card.js
@@ -1,11 +1,3 @@
-// timer
-second = 0;
-minute = 0; 
-hour = 0;
-var timer = document.querySelector(".timer");
-timer.innerHTML = "0 mins 0 secs";
-clearInterval(interval);
-
 // cards array holds all cards
 let card = document.getElementsByClassName("card");
 let cards = [...card];
@@ -13,6 +5,15 @@ let cards = [...card];
 // deck of all cards in game
 const deck = document.getElementById("cards-in-deck");
 
+// declaring move variable
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+// declaring variable of matchedCards
+let matchedCard = document.getElementsByClassName("match");
+
+//array for opened cards
+var openedCards = [];
 
 //opens and shows cards
 var cardDisplay = function (){
@@ -21,3 +22,172 @@ var cardDisplay = function (){
     this.classList.toggle("disabled");
 }
 
+//add openedcards to list and check if cards are matched or not
+function cardOpen() {
+    openedCards.push(this);
+    var len = openedCards.length;
+    if(len === 2){
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+};
+
+//cards match
+function matched(){
+    openedCards[0].classList.add("match", "disabled");
+    openedCards[1].classList.add("match", "disabled");
+    openedCards[0].classList.remove("show", "open", "no-event");
+    openedCards[1].classList.remove("show", "open", "no-event");
+    openedCards = [];
+}
+
+//cards don't match
+function unmatched(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "no-event","unmatched");
+        openedCards[1].classList.remove("show", "open", "no-event","unmatched");
+        enable();
+        openedCards = [];
+    },1100);
+}
+
+//disable cards
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+//enable cards and diable matched cards ??
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+// count number of moves
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if(moves == 1){
+        second = 0;
+        minute = 0; 
+        hour = 0;
+        startTimer();
+    }
+    // setting rates based on moves
+    if (moves > 8 && moves < 12){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves > 13){
+        for( i= 0; i < 3; i++){
+            if(i > 0){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
+
+//game timer
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
+
+
+
+//declaring variable of matchedcards
+let matchedCard = document.getElementsByClassName("match");
+
+
+//shuffle cards
+function shuffle(array) {
+    var current = array.length, temp, random;
+
+    while (current !== 0) {
+        random = Math.floor(Math.random() * current);
+        current -= 1;
+        temp = array[current];
+        array[current] = array[random];
+        array[random] = temp;
+    }
+    return array;
+};
+
+//shuffles cards when reloaded
+document.body.onload = startGame();
+
+//function to start a game
+function startGame(){
+ 
+    // empty the array for opened cards
+    openedCards = [];
+
+    // shuffle deck
+    cards = shuffle(cards);
+    // remove all exisiting classes from each card
+    for (var i = 0; i < cards.length; i++){
+        deck.innerHTML = "";
+        [].forEach.call(cards, function(item) {
+            deck.appendChild(item);
+        });
+        cards[i].classList.remove("show", "open", "match", "disabled");
+    }
+    // reset moves
+    moves = 0;
+    counter.innerHTML = moves;
+    // reset rating
+    for (var i= 0; i < stars.length; i++){
+        stars[i].style.color = "yellow";
+        stars[i].style.visibility = "visible";
+    }
+    //reset timer
+    second = 0;
+    minute = 0; 
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+}
+
+//replay
+function playAgain(){
+    modal.classList.remove("show");
+    startGame();
+}
+
+//add event listeners to each card
+for (var i = 0; i < cards.length; i++){
+    card = cards[i];
+    card.addEventListener("click", displayCard);
+    card.addEventListener("click", cardOpen);
+    card.addEventListener("click",congratulations);
+};
