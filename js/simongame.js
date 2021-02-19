d5e935e26f0340f5463338e71f2bf985b623d78b
JieShengNP <s10205049@connect.np.edu.sg>
2021-02-19 18:17:38 +0800

    Add 'removal' of button on click

--- js/simongame.js
+++ js/simongame.js
@@ -1,3 +1,14 @@
 setInterval(function(){
     $('.square').height($('.square').width());
 }, 100)
+
+let sequence = [];
+let playerSequence = [];
+
+function sgStartGame() {
+    $('#sg-button').prop('disabled', true)
+    $('#sg-button').text("Starting.....")
+    setTimeout(() => {
+        $('#sg-button').css('opacity', 0)
+    }, 1000);
+}
